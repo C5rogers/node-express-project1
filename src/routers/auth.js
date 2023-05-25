8b729c90e291a92912f9587a23924ec71e47057e
c5rogers <natenaelhaylemariam@gmail.com>
2023-05-25 15:07:53 -0400

    some modification

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,8 +1,12 @@
+// this is also requiring the router and response from the express app to use it latter
 const { Router, response } = require('express')
 
 const router = Router()
 
+// this route will authenticate the user based some conditions 
+// and it is also fake authentication because of you dont set up mongodb that you can manipulate it letter and also make it more smartter
 router.post('/login', (request, response) => {
+    // this will deconstract the username and password from the request body 
     const { username, password } = request.body
     if (username && password) {
         if (request.session.user) {
